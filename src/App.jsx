09674586cbe01ecbea260e09b59e04f7bea6249c
= <karimsharawy7@gmial.com>
2023-05-03 12:25:49 -0500

    fix incompatibility Browser ✅

--- src/App.jsx
+++ src/App.jsx
@@ -17,37 +17,40 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 export function Session() {
   return (
     <>
-    <img className='bg1' src={bg_1} alt="" />
-    <img className='bg2' src={bg_2} alt="" />
-    <img className='bg5' src={bg_4} alt="" /> 
-    <img className='bg6' src={bg_4} alt="" /> 
-    <form action='/Inversion' method="get">
-    <div className='Box'>
-    <img className='bg3' src={bg_3} alt="" />    
-      <div className='box-1'>
-        <div className="sufiBox">
-            <img className='img258' src={logo} alt=""/>
-            <p className='Sufp'>Sufipay</p>
-            <p className='A-co'>Administrador comercial</p>
+    <div className="Card1">
+      
+        <img className='bg1' src={bg_1} alt="" />
+        <img className='bg2' src={bg_2} alt="" />
+        <img className='bg5' src={bg_4} alt="" /> 
+        <img className='bg6' src={bg_4} alt="" /> 
+        <div className='Box'>
+            <form action='/Inversion' method="get">
+                <img className='bg3' src={bg_3} alt="" />
+                  <div className='box-1'>
+                    <div className="sufiBox">
+                        <img className='img258' src={logo} alt=""/>
+                        <p className='Sufp'>Sufipay</p>
+                        <p className='A-co'>Administrador comercial</p>
+                    </div>
+                    <div className="box-2">
+                      <div className='box-3'>
+                        <input placeholder=' ' type="text" className="InL1"  required/>
+                        <label htmlFor="" className="Animation">Usuario</label>
+                      </div>
+                      <div className='box-3'>
+                        <input placeholder=' ' type="password" className="InL1" required/>
+                        <label htmlFor="" className="Animation">Contraseña</label>
+                      </div>
+                    </div>
+                    <div className="btn-1">
+                      <input type="submit" value='INGRESAR'/>
+                    </div>
+                    <a className='Forgot' href="">No recuerdo mi contraseña</a>
+                  </div>
+                <img className='bg4' src={bg_3} alt="" />
+            </form>
         </div>
-        <div className="box-2">
-          <div className='box-3'>
-            <input type="text" required/>
-            <label htmlFor="">Usuario</label>
-          </div>
-          <div className='box-3'>
-            <input type="password"required/>
-            <label htmlFor="">Contraseña</label>
-          </div>
-        </div>
-        <div className="btn-1">
-          <input type="submit" value='INGRESAR'/>
-        </div>
-        <a className='Forgot' href="">No recuerdo mi contraseña</a>
-      </div>
-    <img className='bg4' src={bg_3} alt="" />    
     </div>
-    </form>
 
 
     </>
@@ -78,17 +81,16 @@ export function Inversion() {
     const valor = e.target.value;
     setIsop(valor || searchNumberDoc || DB_1 || searchFecha || searchFecha2);
     setTipoDoc(valor);
-
   }
-    function hand1(e){
+  function hand1(e){
     const valor = e.target.value;
+    
     setIsop(valor || searchTipoDoc || DB_1 || searchFecha || searchFecha2);
-
 
     setNumberDoc(valor);
 
   }
- function hand2(e){
+  function hand2(e){
     const valor = e.target.value;
     setIsop(valor || searchTipoDoc || searchNumberDoc || searchFecha || searchFecha2);
 
@@ -96,14 +98,14 @@ export function Inversion() {
 
   }
 
- function hand3(e){
+  function hand3(e) {
     const valor = e.target.value
     setIsop(valor || searchTipoDoc || searchNumberDoc || DB_1 || searchFecha2);
 
     setFecha(valor);
   }
 
- function hand4(e){
+  function hand4(e) {
     const valor = e.target.value;
     setIsop(valor || searchTipoDoc || searchNumberDoc || DB_1 || searchFecha);
     setFecha2(valor);
@@ -119,87 +121,100 @@ export function Inversion() {
 
         <div className='blockBox'>
             <div className='sticky'>
-
-              <nav className='Barnav nv'>
-                <div></div>
-                <div>
-                  <img className='navlogo' src={logo} alt="" />
-                </div>
-                <div className='boC'>
-                  <form action="/">
+                <nav className='Barnav nv'>
+                  <div></div>
+                  <div>
+                    <img className='navlogo' src={logo} alt="" />
+                  </div>
+                  <div className='boC'>
+                    <form action="/">
                     <input id='ir' type="submit" value="" hidden/>
-                    <label className="txtCerrar" htmlFor="ir">
+                    
+                    <label  className="txtCerrar" htmlFor="ir">
                       Cerrar sesión
                     </label>
 
-                  </form>
-                  <p className='icoSalir'>
-                    <img src={SignOut} alt="" srcset="" />
-                  </p>
-                </div>
-              </nav>
+                    <label htmlFor="ir" className='vs1'></label>
+                    </form>
+                    <img className='icoSalir'  src={SignOut} alt=""/>
+                  </div>
+                </nav>
 
-              <nav className='Barnav2 nv'>
-                <div>
-                  <p>Mis desembolsos</p>
-                </div>
-                <div className='btn3'>
-                  <img src={arrowLines} alt="" />
-                  <h4>Descargar</h4>
-                </div>
-              </nav>
-              <nav className='Barnav3 nv'>
-                {isOP && <div className="Vaciar activo deactivo" id='Vclick' onClick={Vacio}>
-                  X
-                </div>}
-
-                  <div className="doc wg">
-                    <input type="text" id='Lleno' on value={searchTipoDoc} className='InL1'onChange={hand}/>
-                    <label htmlFor="" className='Animation'>Tipo doc.</label>
-                    <label className='arrowB' htmlFor=""></label>
+                <nav className='Barnav2 nv'>
+                  <div>
+                    <p>Mis desembolsos</p>
                   </div>
-                  <div className="Numero wg">
-                    <input type="text" id='Lleno' value={searchNumberDoc} className='InL1' onChange={hand1}/>
-                    <label htmlFor="" className='Animation'>Número de documento</label>
+                  <div className='btn3'>
+                    <img src={arrowLines} alt="" />
+                    <h4>Descargar</h4>
                   </div>
-                  <div className="desembolso wg">
-                    <input type="text" id='Lleno' value={DB_1} className='InL1' onChange={hand2}/>
-                    <label htmlFor="" className='Animation'>Número de desembolso.</label>
-                  </div>
-                  <div className="day wg">
-                    <input type="text" id='Lleno' value={searchFecha} className='InL1' onChange={hand3}/>
-                    <label htmlFor="" className='Animation'>Desde.</label>
-                    <label htmlFor="" className='Calendar'><img src={Calendar} alt="" /></label>
-                  </div>
-                  <div className="hasta wg">
-                    <input type="text" id='Lleno' value={searchFecha2}  className='InL1' onChange={hand4}/>
-                    <label htmlFor="" className='Animation'>Hasta</label>
-                    <label htmlFor="" className='Calendar'><img src={Calendar} alt="" /></label>                   
-                  </div>
-              </nav>
-            </div>
+                </nav>
+                <nav className='Barnav3 nv'>
+                  {isOP && <div className="Vaciar activo" id='Vclick' onClick={Vacio}>
+                    X
+                  </div>}
             
-            <table className='Tbs sticky2'>
+                  <div className="boxIp">
+                    <div className="doc wg">
+                      <input type="text" id='Lleno' on value={searchTipoDoc} className='InL1'onChange={hand} placeholder=' '/>
+                      <label htmlFor="" className='Animation'>Tipo doc.</label>
+                      <label className='arrowB' htmlFor=""></label>
+                    </div>
+                    <div className="Numero wg">
+                      <input type="text" id='Lleno' value={searchNumberDoc} className='InL1' onChange={hand1} placeholder=' '/>
+                      <label htmlFor="" className='Animation'>Número de documento</label>
+                    </div>
+                    <div className="desembolso wg">
+                      <input type="text" id='Lleno' value={DB_1} className='InL1' onChange={hand2} placeholder=' '/>
+                      <label htmlFor="" className='Animation'>Número de desembolso.</label>
+                    </div>
+                    <div className="day wg">
+                      <input type="text" id='Lleno' value={searchFecha} className='InL1' onChange={hand3} placeholder=' '/>
+                      <label htmlFor="" className='Animation'>Desde.</label>
+                      <label htmlFor="" className='Calendar'><img src={Calendar} alt="" /></label>
+                    </div>
+                    <div className="hasta wg">
+                      <input type="text" id='Lleno' value={searchFecha2}  className='InL1' onChange={hand4} placeholder=' '/>
+                      <label htmlFor="" className='Animation'>Hasta</label>
+                      <label htmlFor="" className='Calendar'><img src={Calendar} alt="" /></label>                   
+                    </div>
+                </div>
+              </nav>  
+              <div className='nvtbs'>
+                  <div className="bbs">
+
+                  <p className='F1 Color1'>Fecha y hora</p>
+                  <p className='F2 Color1'>Número de desembolso</p>
+                  <p className='F3 Color1'>Tipo de documento</p>
+                  <p className='F4 Color1'>Número de documento</p>
+                  <p className='F5 Color1'>Monto</p>
+      
+                  </div>
+              </div>
+
+            </div>
+            <div>
+
+            <table className='Tbs'>
               <thead>
-                  <tr>
-                    <th className='F1 Color1'>Fecha y hora</th>
-                    <th className='F2 Color1'>Número de desembolso</th>
-                    <th className='F3 Color1'>Tipo de documento</th>
-                    <th className='F4 Color1'>Número de documento</th>
-                    <th className='F5 Color1'>Monto</th>
+                  <tr hidden>
+                    <th>Fecha y hora</th>
+                    <th>Número de desembolso</th>
+                    <th>Tipo de documento</th>
+                    <th>Número de documento</th>
+                    <th>Monto</th>
                   </tr>
               </thead>
               <tbody>
                 {
-
                   data
                     .filter((e) => {
-                      return (searchFecha.toLowerCase() === ''
+                      return (searchFecha.toString() === ''
                       ? e
                       : e.Fechas.toString().includes(searchFecha)) 
                     })
                     .filter((e) => {
-                      return (searchFecha2.toLowerCase() === ''
+                      return (searchFecha2.toString() === ''
                       ? e
                       : e.Fechas.toString().includes(searchFecha2))
                     })
@@ -209,9 +224,9 @@ export function Inversion() {
                       : e.desembolso.toString().includes(DB_1))
                     })
                     .filter((e)=>{
-                      return (searchTipoDoc.toLowerCase() === ''
+                      return (searchTipoDoc.toString() === ''
                       ? e
-                      : e.Tipo_Documentos.toString().includes(searchTipoDoc))
+                      : e.Tipo_Documentos.toLowerCase().includes(searchTipoDoc))
                     })
                     .filter((e)=>{
                       return (searchNumberDoc.toLowerCase() === ''
@@ -221,16 +236,31 @@ export function Inversion() {
                     .map((x)=>(
                     
                     <tr key={x.id}>
-                    <td>{x.Fechas} {x.Horas}</td>
+                    <td className="td1">
+                        <div className="td1_1">
 
-                    <td>{x.desembolso}</td>
+                        {x.Fechas} {x.Horas}
 
-                    <td>{x.Tipo_Documentos}</td>
+                        </div>
+                      </td>
+
+                    <td className="td2">
+                      <div className="td1_2">
+                      {x.desembolso}
+                      </div>
+                      </td>
+
+                    <td className="td3">
+                      <div className="td1_3">
+
+                      {x.Tipo_Documentos}
+                      </div>
+                      </td>
                     
-                    <td>{x.Numero_Doc}</td>
+                    <td className="td4">{x.Numero_Doc}</td>
 
 
-                    <td>${x.Monto}</td>
+                    <td className="td5">${x.Monto}</td>
                   </tr>
                   ))
                   
@@ -238,6 +268,7 @@ export function Inversion() {
               
               </tbody>
             </table>
+            </div>
 
 
         </div>
@@ -262,7 +293,7 @@ export function Inversion() {
 
 
           <div className='Rg1'>
-            <input type="number" className='InL1' />
+            <input type="number" className='InL1' placeholder=' '/>
             <label className='Animation' htmlFor="">Registros por página</label>
             <label className='arrowB' htmlFor=""></label>
           </div>
